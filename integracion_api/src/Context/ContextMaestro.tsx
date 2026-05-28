610dc64025b905c465bc20cc7463a824b7fde327
Marlon Martinez <alex2015drum@gmail.com>
2026-05-27 21:20:03 -0600

    delete

--- integracion_api/src/Context/ContextMaestro.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { IMaestros } from "../Modelo/IMaestro";
-
-export const ContextMaestro = createContext({
-    maestros:[] as IMaestros[],
-    guardarmaestro:()=>{},
-    editarMaestro:()=>{},
-    eliminarMaestro:()=>{}
-})
